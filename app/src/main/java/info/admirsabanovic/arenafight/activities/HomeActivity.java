e8e8efa50b9bf911c61fc8ae88de1ede2a91bcfc
Admir Sabanovic <critical@linux-jysu.site>
2015-05-07 07:34:00 +0200

    set up home activity

--- app/src/main/java/info/admirsabanovic/arenafight/activities/HomeActivity.java
+++ app/src/main/java/info/admirsabanovic/arenafight/activities/HomeActivity.java
@@ -2,39 +2,63 @@ package info.admirsabanovic.arenafight.activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import info.admirsabanovic.arenafight.R;
 
 public class HomeActivity extends ActionBarActivity {
 
+    TextView onlinePlayers;
+    JSONObject obj;
+    Map<String, Integer> characterImageMap;
+    ImageView characterHolder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        try {
+            obj = new JSONObject(getIntent().getStringExtra("json"));
+            updateWorldStatistics(obj);
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+    //1 human 2 orc
+    //1 mage 2 warrior
+    void initialResources(){
+        onlinePlayers = (TextView)findViewById(R.id.txtOnlinePlayers);
+        characterImageMap = new HashMap<String, Integer>();
+        characterImageMap.put("humanmage", R.drawable.humanmage);
+        characterImageMap.put("humanwarrior", R.drawable.humanwarrior);
+        characterImageMap.put("orcmage", R.drawable.orcmage);
+        characterImageMap.put("orcwarrior", R.drawable.orcwarrior);
+        characterHolder = (ImageView)findViewById(R.id.characterHolder);
+    }
+
+    void updateWorldStatistics(JSONObject data) throws JSONException {
+        int numberOnlinePlayers = data.getInt("numUsers");
+        onlinePlayers.setText(numberOnlinePlayers);
+        int race = data.getInt("race");
+        int clas = data.getInt("class");
+        setCharacterHolder(race, clas);
+    }
+
+    void setCharacterHolder(int r, int c){
+        if(r == 1 && c == 1)
+            characterHolder.setImageResource(characterImageMap.get("humanmage"));
+        else if(r == 1 && c == 2)
+            characterHolder.setImageResource(characterImageMap.get("humanwarrior"));
+        else if (r == 2 && c == 1)
+            characterHolder.setImageResource(characterImageMap.get("orcmage"));
+        else if (r == 2 && c == 2)
+            characterHolder.setImageResource(characterImageMap.get("orcwarrior"));
+    }
+
 }
